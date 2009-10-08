c8d07baeb6fdfe547e66e67f9388d2495b0559c1
ekilfoil <ekilfoil@f654edcb-a5fb-4f28-a0e7-68c327795dae>
2009-10-08 01:26:54 +0000

    added GPL info to all source files
    git-svn-id: http://svn.mangler.org/mangler/trunk@31 f654edcb-a5fb-4f28-a0e7-68c327795dae

--- src/channeltree.cpp
+++ src/channeltree.cpp
@@ -1,5 +1,27 @@
 /*
  * vim: softtabstop=4 shiftwidth=4 cindent foldmethod=marker expandtab
+ *
+ * $LastChangedDate: 2009-10-01 13:25:43 -0700 (Thu, 01 Oct 2009) $
+ * $Revision: 504 $
+ * $LastChangedBy: ekilfoil $
+ * $URL: http://svn.manglerproject.net/svn/mangler/trunk/libventrilo3/libventrilo3.h $
+ *
+ * Copyright 2009 Eric Kilfoil 
+ *
+ * This file is part of Mangler.
+ *
+ * Mangler is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Mangler is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Mangler.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "iostream"
