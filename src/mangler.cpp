de89253312b10feee07e5cd85c1827c04bc941c9
ekilfoil <ekilfoil@f654edcb-a5fb-4f28-a0e7-68c327795dae>
2009-11-16 01:30:07 +0000

    for real.... auto-rejoin chat if the chat window is open on connect
    git-svn-id: http://svn.mangler.org/mangler/trunk@299 f654edcb-a5fb-4f28-a0e7-68c327795dae

--- src/mangler.cpp
+++ src/mangler.cpp
@@ -612,29 +612,7 @@ bool Mangler::getNetworkEvent() {/*{{{*/
                     }
                     builder->get_widget("chatWindow", window);
                     if(window->get_visible()) {
-                        window->show();
-                        chat->addUser(v3_get_user_id());
-                        u = v3_get_user(v3_get_user_id());
-                        if (!u) {
-                            fprintf(stderr, "couldn't retreive user id %d\n", ev->user.id);
-                            break;
-                        }
-                        //fprintf(stderr, "updating user id %d: %s in channel %d\n", ev->user.id, u->name, ev->channel.id);
-                        // we cannot remove the lobby user, so bail out when the server comment is updated. See ticket #30
-                        if (u->id == 0) {
-                            channelTree->updateLobby(c_to_ustring(u->name), c_to_ustring(u->comment), u->phonetic);
-                        } else {
-                            channelTree->updateUser(
-                                    (uint32_t)u->id,
-                                    (uint32_t)ev->channel.id,
-                                    c_to_ustring(u->name),
-                                    c_to_ustring(u->comment),
-                                    u->phonetic,
-                                    u->url,
-                                    c_to_ustring(u->integration_text),
-                                    (bool)u->guest);
-                        }
-                        v3_free_user(u);
+                        v3_join_chat();
                     }
                 }
                 break;/*}}}*/
