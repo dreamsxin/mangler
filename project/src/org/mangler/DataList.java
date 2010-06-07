6c7bccc06568a4f13574c9bc9f0cc09655dfd9eb
clearscreen <clearscreen@f654edcb-a5fb-4f28-a0e7-68c327795dae>
2010-06-07 01:01:20 +0000

    Java equals() shenanigans.
    git-svn-id: http://svn.mangler.org/mangler/branches/android@885 f654edcb-a5fb-4f28-a0e7-68c327795dae

--- project/src/org/mangler/DataList.java
+++ project/src/org/mangler/DataList.java
@@ -14,7 +14,7 @@ public class DataList<T> {
 	
 	protected static void deleteByMatch(String index, Object value) {
 		for(Iterator<HashMap<String, Object>> iterator = data.iterator(); iterator.hasNext(); ) {
-			if((Short)iterator.next().get(index) == value) {
+			if(((Short)iterator.next().get(index)).equals(value)) {
 				iterator.remove();
 				return;
 			}
