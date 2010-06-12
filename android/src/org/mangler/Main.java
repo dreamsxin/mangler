58efb73c47d382b58813619b40f23c1f88e9d4df
ekilfoil <ekilfoil@f654edcb-a5fb-4f28-a0e7-68c327795dae>
2010-06-12 21:11:43 +0000

    auto-scale the logo for people with crappy phones that have tiny screens
    git-svn-id: http://svn.mangler.org/mangler/trunk@915 f654edcb-a5fb-4f28-a0e7-68c327795dae

--- android/src/org/mangler/Main.java
+++ android/src/org/mangler/Main.java
@@ -29,20 +29,35 @@ import android.view.View;
 import android.view.WindowManager;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class Main extends Activity {
 
-	protected boolean isPortrait;
+	private boolean isPortrait;
+	private int screenHeight;
+	private int screenWidth;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getWindowManager().getDefaultDisplay().getWidth() < getWindowManager().getDefaultDisplay().getHeight()) {
+        screenWidth = getWindowManager().getDefaultDisplay().getWidth();
+        screenHeight = getWindowManager().getDefaultDisplay().getHeight();
+        if (screenWidth < screenHeight) {
         	isPortrait = true;
         	setContentView(R.layout.main_portrait);
+        	ImageView logo = (ImageView) findViewById(R.id.main_portrait_logo);
+        	logo.setAdjustViewBounds(true);
+        	logo.setMaxHeight((int) (screenHeight/2.5));
+        	logo.setMaxWidth((int) (screenHeight/2.5));
+        	logo.setPadding(0, screenHeight/25, 0, screenHeight/25);
         } else {
         	isPortrait = false;
         	setContentView(R.layout.main_landscape);
+        	ImageView logo = (ImageView) findViewById(R.id.main_landscape_logo);
+        	logo.setAdjustViewBounds(true);
+        	logo.setMaxHeight(screenWidth/2);
+        	logo.setMaxWidth(screenWidth/2);
+        	logo.setPadding(screenWidth/25, screenWidth/25, screenWidth/25, screenWidth/25);
         }
 
         // Volume controls.
